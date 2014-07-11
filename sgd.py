419fc92565c64c453bb2566e9cd9fb82a560901a
Brooke Osborn <dsullivan7@hotmail.com>
2014-07-11 17:13:29 +0200

    asgd now working more or less

--- sgd.py
+++ sgd.py
@@ -1,6 +1,9 @@
 import numpy as np
-import pdb
+import math
 from scipy import linalg
+
+# remove after debugging
+import pdb
 
 
 class NewSGD():
@@ -24,7 +27,8 @@ class NewSGD():
 
     def _get_loss_function(self, loss):
         return {"SquaredLoss": SquaredLoss,
-                "Hinge": Hinge
+                "Hinge": Hinge,
+                "Log": Log
                 }[loss]()
 
     def _fit_regressor(self, X, y, loss, learning_rate, n_iter):
@@ -34,14 +38,14 @@ class NewSGD():
         learning_rate_type = learning_rate
 
         # components for asgd
-        avg_weights = np.zeros(X.shape[1])
         total_iter = 0
+        avg_weights = np.zeros(X.shape[1])
         pobj = []
 
         # iterate according to the number of iterations specified
         for n in range(n_iter):
             for i in range(X.shape[0]):
-
+                total_iter += 1
                 # base sgd code
                 p = np.dot(X[i], weights)
                 update = loss_function.dloss(p, y[i])
@@ -49,19 +53,26 @@ class NewSGD():
 
                 # asgd
                 if self.avg:
-                    # pdb.set_trace()
-                    avg_weights *= total_iter
+                    avg_weights *= total_iter - 1
                     avg_weights += weights
-                    avg_weights /= (total_iter + 1)
-                    weights = np.copy(avg_weights)
-                    total_iter += 1
+                    avg_weights /= total_iter
 
-                loss = 0.5 * linalg.norm(y - np.dot(X, weights)) ** 2
-                pobj.append(loss)
+                # loss calculation
+                if self.avg:
+                    pobj.append(sum(map(loss_function.loss,
+                                        np.dot(X, avg_weights),
+                                        y)))
+                else:
+                    pobj.append(sum(map(loss_function.loss,
+                                        np.dot(X, weights),
+                                        y)))
 
         # set the corresponding private values
         self.pobj_ = pobj
-        self.coef_ = weights
+        if self.avg:
+            self.coef_ = avg_weights
+        else:
+            self.coef_ = weights
 
 
 class Hinge():
@@ -70,56 +81,92 @@ class Hinge():
 
     def loss(self, p, y):
         z = p * y
-        return self.threshold - z if z <= threshold else return 0
+        if z <= self.threshold:
+            return self.threshold - z
+        else:
+            return 0
 
     def dloss(self, p, y):
         z = p * y
-        return -y if z <= threshold else return 0
+        if z <= self.threshold:
+            return -y
+        else:
+            return 0
 
 
 class SquaredLoss():
     def loss(self, p, y):
-        return .5 * (p - y) * (p - y)
+        return .5 * (p - y) ** 2
 
     def dloss(self, p, y):
         return p - y
 
 
+class Log():
+
+    def loss(self, p, y):
+        z = p * y
+        return math.log(1 + math.exp(-z))
+
+    def dloss(self, p, y):
+        z = p * y
+        return -y / (math.exp(z) + 1.0)
+
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
+    import pandas as p
     import naive_asgd
     import experimental_asgd
 
+    iterations = 2
+
+    # """
     rng = np.random.RandomState(42)
-    n_samples, n_features = 1000, 10
+    n_samples, n_features = 5000, 10
 
     X = rng.normal(size=(n_samples, n_features))
     w = rng.normal(size=(n_features,))
 
     # Define a ground truth on the scaled data
+    def svm(x):
+        return 1 if x > 1 else -1
     y = np.dot(X, w)
+    y = map(svm, y)
+    # """
 
-    iterations = 5
+    """
+    nrows = 100000
+    X = np.array(p.read_table('../leon_sgd/sgd/data/pascal/alpha_train.dat',
+                              sep=" ",
+                              nrows=nrows,
+                              header=None))[:,:-1]
+    y = np.array(p.read_table('../leon_sgd/sgd/data/pascal/alpha_train.lab',
+                              nrows=nrows,
+                              header=None))
+    """
 
     model = NewSGD('Hinge', eta0=.01, n_iter=iterations, avg=False)
     model.fit(X, y)
 
-    avg_model = NewSGD('Hinge', eta0=1., n_iter=iterations, avg=True)
+    avg_model = NewSGD('Hinge', eta0=.1, n_iter=iterations, avg=True)
     avg_model.fit(X, y)
 
+    """
     npinto_model = naive_asgd.NaiveBinaryASGD(n_features,
                                               sgd_step_size0=.01,
                                               n_iterations=iterations)
-    npinto_model.fit(X, y)
+    """
+    # npinto_model.fit(np.array(X), np.array(y))
 
-    npinto_model2 = experimental_asgd.ExperimentalBinaryASGD(n_features)
-    npinto_model2.fit(X, y)
+    # npinto_model2 = experimental_asgd.ExperimentalBinaryASGD(n_features)
+    # npinto_model2.fit(X, y)
 
     plt.close('all')
     plt.plot(np.log10(model.pobj_), label='SGD')
     plt.plot(np.log10(avg_model.pobj_), label='ASGD')
-    plt.plot(np.log10(npinto_model.pobj_), label='NPINTO')
-    plt.plot(np.log10(npinto_model2.pobj_), label='NPINTO2')
+    # plt.plot(np.log10(npinto_model.pobj_), label='NPINTO')
+    # plt.plot(np.log10(npinto_model2.pobj_), label='NPINTO2')
     plt.xlabel('iter')
     plt.ylabel('cost')
     plt.legend()

f549d0520b0301190a614332d03c9d6967f3b58b
Brooke Osborn <dsullivan7@hotmail.com>
2014-07-15 18:14:27 +0200

    adding adagrad

--- learning_rates.py
+++ learning_rates.py
@@ -3,7 +3,8 @@ from abc import ABCMeta, abstractmethod
 
 def get_learning_rate(learning_rate_type, eta0=.01):
     return {"static": Static,
-            "exponential": Exponential
+            "exponential": Exponential,
+            "adagrad": AdaGrad
             }[learning_rate_type.lower()](eta0)
 
 
@@ -14,15 +15,25 @@ class BaseLearningRate(object):
         self.eta0 = eta0
 
     @abstractmethod
-    def step(self, num_iter):
+    def step(self, num_iter=None, gradient=None):
         pass
 
 
 class Static(BaseLearningRate):
-    def step(self, num_iter):
+    def step(self, num_iter=None, gradient=None):
         return self.eta0
 
 
 class Exponential(BaseLearningRate):
-    def step(self, num_iter):
+    def step(self, num_iter, gradient=None):
         return (1. + .02 * num_iter) ** (-2. / 3.)
+
+
+class AdaGrad(BaseLearningRate):
+    def __init__(self, eta0):
+        self.eta0 = eta0
+        self.sum_squared_grad = 0
+
+    def step(self, gradient, num_iter=None):
+        self.sum_squared_grad += gradient ** 2
+        return self.eta0 / (self.sum_squared_grad ** (1./2.))
--- sgd.py
+++ sgd.py
@@ -65,7 +65,6 @@ class NewSGD():
         loss_function = loss_functions.get_loss_function(loss)
         total_iter = self.total_iter_
         pobj = []  # stores total loss for each iteration
-        step = self.learning_rate.step(total_iter)
 
         # components for asgd
         if self.avg:
@@ -80,6 +79,8 @@ class NewSGD():
                 # base sgd code
                 p = np.dot(X[i], weights)
                 update = loss_function.dloss(p, y[i])
+                step = self.learning_rate.step(num_iter=total_iter,
+                                               gradient=update)
                 weights -= step * update * X[i]
 
                 # averaged sgd
@@ -87,9 +88,6 @@ class NewSGD():
                     avg_weights *= total_iter - 1
                     avg_weights += weights
                     avg_weights /= total_iter
-
-                # update learning rate
-                step = self.learning_rate.step(total_iter)
 
                 # loss calculation
                 if self.avg:
@@ -115,10 +113,10 @@ if __name__ == '__main__':
 
     # """
     rng = np.random.RandomState(42)
-    n_samples, n_features = 10000, 100
+    n_samples, n_features = 10000, 10
 
     X = rng.normal(size=(n_samples, n_features))
-    w = rng.normal(size=(n_features,))
+    w = rng.normal(size=n_features)
 
     # Define a ground truth on the scaled data
     y = np.dot(X, w)
@@ -152,9 +150,18 @@ if __name__ == '__main__':
                        learning_rate_type='static',
                        n_iter=iterations,
                        avg=True)
+
     for x_chunk, y_chunk in zip(x_chunks, y_chunks):
         avg_model.partial_fit(x_chunk, y_chunk)
 
+    adagrad_model = NewSGD('hinge',
+                           eta0=10.,
+                           learning_rate_type='adagrad',
+                           n_iter=iterations,
+                           avg=True)
+
+    for x_chunk, y_chunk in zip(x_chunks, y_chunks):
+        adagrad_model.partial_fit(x_chunk, y_chunk)
     """
     npinto_model = naive_asgd.NaiveBinaryASGD(n_features,
                                               sgd_step_size0=.01,
@@ -165,6 +172,7 @@ if __name__ == '__main__':
     plt.close('all')
     plt.plot(np.log10(model.pobj_), label='SGD')
     plt.plot(np.log10(avg_model.pobj_), label='ASGD')
+    plt.plot(np.log10(adagrad_model.pobj_), label='ADAGRAD')
     # plt.plot(np.log10(npinto_model.pobj_), label='NPINTO')
     plt.xlabel('iter')
     plt.ylabel('cost')

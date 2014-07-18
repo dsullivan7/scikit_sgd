25098eeb3606d141a925d67b203a67e8f6a388bf
Brooke Osborn <dsullivan7@hotmail.com>
2014-07-18 10:41:32 +0200

    changing from hard coded loss function to calling external loss function

--- sgd_opt.pyx
+++ sgd_opt.pyx
@@ -12,13 +12,7 @@ def partial_fit(X, y, n_iter, eta0):
         # iterate over each sample in X
         for i in range(X.shape[0]):
             p = np.dot(X[i], weights)
-            
-            z = p * y[i]
-            step = 0
-            if z <= 1.0:
-                 step = -y[i]
-            gradient = step * X[i]
-            #gradient = loss_function.dloss(p, y[i]) * X[i]
+            gradient = loss_function.dloss(p, y[i]) * X[i]
             weights += -eta0 * gradient
 
     return weights

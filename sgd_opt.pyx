3478ba4a5956c8fb1024eda4d13a1adb40977379
Brooke Osborn <dsullivan7@hotmail.com>
2014-07-22 14:55:07 +0200

    removed all for loops, now uses all cblas

--- sgd_opt.pyx
+++ sgd_opt.pyx
@@ -1,13 +1,29 @@
 cimport loss_functions
+from libc.stdlib cimport malloc
 import numpy as np
 cimport numpy as np
 
 # import cblas module
 cdef extern from "cblas.h":
-    double ddot "cblas_ddot"(int, double*, int, double*, int) nogil
+    
+    # dot product of two n elemenet vectors x and y
+    double ddot "cblas_ddot"(int n, double* x, int incrx, double* y, int incry) nogil
+    
+    # scale the passed in n element vector x at scale
+    void dscal "cblas_dscal"(int n, double scale, double* x, int incrx) nogil
+    
+    # copies an n element vector x to another vector y
+    void dcopy "cblas_dcopy"(int n, double* x, int incrx, double* y, int incry) nogil
+    
+    # adds a vector x * scaler scale to another vector y
+    void daxpy "cblas_daxpy"(int n, double scale, double* x, int incrx, double* y, int incry) nogil 
 
 # the entry point from python into the cython
-def partial_fit(np.ndarray[double, ndim=2, mode="c"] X, np.ndarray[double, ndim=1, mode="c"]  y, np.ndarray[double, ndim=1, mode="c"]  weights, int n_iter, double eta0):
+def partial_fit(np.ndarray[double, ndim=2, mode="c"] X,
+                np.ndarray[double, ndim=1, mode="c"] y,
+                np.ndarray[double, ndim=1, mode="c"] weights,
+                int n_iter,
+                double eta0):
     _partial_fit(&X[0, 0], &y[0], &weights[0], n_iter, eta0, X.shape[0], X.shape[1])
 
 cdef void _partial_fit(double* X, double* y, double* weights, int n_iter, double eta0, int n_samples, int n_features):
@@ -17,6 +33,8 @@ cdef void _partial_fit(double* X, double* y, double* weights, int n_iter, double
     cdef int j
     cdef double p
     cdef double gradient
+    cdef int incr = 1
+    cdef double* x_entry = <double*> malloc(n_features * sizeof(double))
     
     cdef loss_functions.Hinge loss_function = loss_functions.get_loss_function()
     loss_function.set_threshold(1.0)
@@ -28,11 +46,15 @@ cdef void _partial_fit(double* X, double* y, double* weights, int n_iter, double
         # iterate over each sample in X
         for i in range(n_samples):
             total_iter = i * n_features
+            
+            # copy over the entry of x
+            dcopy(n_features, X + total_iter, incr, x_entry, incr) 
 
             # compute the dot product of the weights and the current sample
-            p = ddot(n_features, weights, 1, X + total_iter, 1)
-            
-            # compute the gradient and update the weights
-            for j in range(n_features):
-                gradient = loss_function.dloss(p, y[i]) * X[total_iter + j]
-                weights[j] = weights[j] - (eta0 * gradient)
+            p = ddot(n_features, weights, incr, x_entry, incr)
+           
+            # scale the x_entry vector to the result of the dloss function
+            dscal(n_features, eta0 * loss_function.dloss(p, y[i]), x_entry, incr)
+
+            # update the weight vector
+            daxpy(n_features, -1, x_entry, incr, weights, incr)
